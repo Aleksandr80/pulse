11f0760871d8b151846d8e612d73dc1a781ed742
Aleksandr <example@gmail.com>
2021-01-13 12:35:26 +0200

    three screen commit

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,8 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    // ** - следить за всеми папками внутри
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         // .pipe(sass())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
@@ -29,7 +30,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"))
 });
 gulp.watch("src/*.html").on('change', browserSync.reload);
 // Запускаем обе задачи одной командой - таском
