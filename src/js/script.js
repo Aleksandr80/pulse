7d386c463f890a74bbd266fc949ea9dc2468393a
Aleksandr <example@gmail.com>
2021-01-15 19:40:40 +0200

    feed

--- src/js/script.js
+++ src/js/script.js
@@ -67,6 +67,50 @@ document.querySelector('.next').addEventListener('click',function () {
     slider.goTo('next');
 });
 
+
+// Делаем переключение табов на jquery
+$(document).ready(function(){
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+// Делаем переключение по ссылкам Подробнее и Назад
+// $('.catalog-item__link').each(function(i) {
+//     $(this).on('click', function(e) {
+//         // Очищаем поведение браузера по умолчанию по отношению к ссылке 
+//         e.preventDefault();
+//         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+//         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+//     })
+// })
+
+// $('.catalog-item__back').each(function(i) {
+//     $(this).on('click', function(e) {
+//         // Очищаем поведение браузера по умолчанию по отношению к ссылке 
+//         e.preventDefault();
+//         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+//         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+//     })
+// })
+
+// Оптимизируем код переключение контента выше
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            // Очищаем поведение браузера по умолчанию по отношению к ссылке 
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    })
+}
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
+});
 // window.addEventListener('DOMContentLoaded', () => {
 //     const menu = document.querySelector('.menu'),
 //     menuItem = document.querySelectorAll('.menu_item'),
