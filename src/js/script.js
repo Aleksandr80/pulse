b11b4e49983b516f4aaa9ff7835f9c94e1bc227d
Aleksandr <example@gmail.com>
2021-01-19 21:30:40 +0200

    modal_window+adaptiv

--- src/js/script.js
+++ src/js/script.js
@@ -110,21 +110,83 @@ function toggleSlide(item) {
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
 
+//Modal 
+// Для красивого анимированного скрытия элементов со страницы исп. fadeOut
+// $('[data-modal=consultation]').fadeOut();
+
+    $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    // $('.button_mini').on('click', function(){
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+    $('.button_mini').each(function(i) {
+        $(this).on('click',function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    // Validation forms
+
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //             }
+    //         },
+    //         messages: {
+    //             name: {
+    //                 required: "Пожалуйста, введите своё имя",
+    //                 minlength: jQuery.validator.format("Введите {0} символа!")
+    //             },
+    //             phone: "Пожалуйста, введите свой номер телефона",
+    //             email: {
+    //               required: "Пожалуйста, введите свою почту",
+    //               email: "Неправильно введён адрес почты"
+    //             }
+    //           }
+    // });
+    // $('#order form').validate();
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Пожалуйста, введите своё имя",
+                        minlength: jQuery.validator.format("Введите {0} символа!")
+                    },
+                    phone: "Пожалуйста, введите свой номер телефона",
+                    email: {
+                      required: "Пожалуйста, введите свою почту",
+                      email: "Неправильно введён адрес почты"
+                    }
+                  }
+        });
+    };
+    
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
 });
-// window.addEventListener('DOMContentLoaded', () => {
-//     const menu = document.querySelector('.menu'),
-//     menuItem = document.querySelectorAll('.menu_item'),
-//     hamburger = document.querySelector('.hamburger');
-
-//     hamburger.addEventListener('click', () => {
-//         hamburger.classList.toggle('hamburger_active');
-//         menu.classList.toggle('menu_active');
-//     });
-
-//     menuItem.forEach(item => {
-//         item.addEventListener('click', () => {
-//             hamburger.classList.toggle('hamburger_active');
-//             menu.classList.toggle('menu_active');
-//         })
-//     })
-// })
